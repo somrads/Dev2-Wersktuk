b721f58ddc4dec8f19214c1fdbef10f5c0ac2588
somrad.sharma@student.ehb.be <somrad.sharma@student.ehb.be>
2020-05-12 05:50:49 +0200

    alles af

--- index.js
+++ index.js
@@ -1,42 +1,132 @@
 "use strict";
+// GLOBAL VARIABLES ///
 let allMovieData;
-let allPosterData;
+
+// FIREBASE ///
+
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyD04u2sMUv-aSD4XE7KbjtXdEeNF_If4r8",
+  authDomain: "dev-2-18d9a.firebaseapp.com",
+  projectId: "dev-2-18d9a",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth.Auth.Persistence.LOCAL;
+
+const database = firebase.firestore();
+const watchedMoviesCollection = database.collection("watched-movies");
+
+
+const convertQuerySnapshotToRegularArray = (querySnapshot) => querySnapshot.docs.map((item) => ({
+  id: item.id,
+  ...item.data()
+}));
+////// ///////
+
+
+
+//// LOG IN  & SIGN UP PAGE /////
+
+// LOG IN//
+let login = document.getElementById("btn-login");
+if (login) {
+  login.addEventListener('click', logInButton);
+}
+
+async function logInButton() {
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+
+  if (email != "" && password != "") {
+    let result = firebase.auth().signInWithEmailAndPassword(email, password);
+    result.catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+
+      console.log(errorCode);
+      console.log(errorMessage);
+      window.alert(errorMessage);
+    });
+  } else {
+    window.alert("Please fill out all fields");
+
+  }
+
+}
+
+async function buttons() {
+  let logout = document.getElementById("btn-logout"); // log out
+  let signUp = document.getElementById("btn-signup"); // sign up
+  if (logout) {
+    logout.addEventListener('click', logOutButton);
+  }
+  if (signUp) {
+    signUp.addEventListener('click', signUpButton);
+  }
+
+}
+
+async function logOutButton() {
+  firebase.auth().signOut();
+
+}
+
+async function signUpButton() {
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+  let confirmPassword = document.querySelector("#confirmPassword").value;
+
+  if (email != "" && password != "" && confirmPassword != "") {
+    if (password == confirmPassword) {
+      let result = firebase.auth().createUserWithEmailAndPassword(email, password);
+      result.catch(function (error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+        window.alert(errorMessage);
+      });
+    } else {
+      window.alert("Password do not match with Confirm Password");
+
+    }
+
+  } else {
+    window.alert("Please fill out all fields");
+
+  }
+}
+//// //// //// //////////////////////////////
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   await getMovieData();
   await movieOptionTags(allMovieData);
-
-
+  await watchedMovies();
+  await buttons();
 
   //MOVIE DETAILS
   const selectTag = document.querySelector('select');
   selectTag.onchange = () => {
-
     for (let i = 0; i < allMovieData.length; i++)
       if (allMovieData[i].title === selectTag.value) {
         selectedMovieDetails(allMovieData[i]);
         return;
       }
   };
-
-
-  // COMMENTS
-  const submitButton = document.querySelector('#submit-btn');
-
-  if (submitButton) {
-    submitButton.addEventListener('click', () => {
-      const text = document.querySelector('#input-txt').value;
-      addListItem(text);
-    });
-  }
-
 });
-// MOVIE DATA
+
+
+////// MOVIE DATA ///////
+
+///// FETCH CALL
+
 const getMovieData = async () => {
   try {
     const movieData = await fetch("https://ghibliapi.herokuapp.com/films");
     await movieData.json()
-      .then()
       .then(data => {
         allMovieData = data;
       });
@@ -44,61 +134,82 @@ const getMovieData = async () => {
     console.error(err);
   }
 };
-/*
-// POSTER DATA
-const getPosterData = async (film) => {
-  try {
-    const posterData = await fetch("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?");
-    await posterData.json()
-      .then(data => {
-        allPosterData = data;
-      });
-  } catch (err) {
-    console.error(err);
-  }
-};
-*/
 
 
-const movieOptionTags = (movieObj) => {
-  for (let i = 0; i < movieObj.length; i++) {
+/// OPTION TAGS 
+const movieOptionTags = (moviesArr) => {
+  moviesArr.forEach(element => {
     const optionTag = document.createElement('option');
-    optionTag.value = movieObj[i].title;
-    optionTag.innerText = movieObj[i].title;
+    optionTag.value = element.title;
+    optionTag.innerText = element.title;
     document.querySelector('#select-movies').appendChild(optionTag);
-  }
+  });
 };
 
 
 // MOVIE DETAILS
-
 const selectedMovieDetails = (selectedMovie) => {
-  const movieDetailTag = document.querySelector('#movie-details');
-
-  movieDetailTag.innerHTML = '';
-
-  const title = document.createElement('h3');
-  const image = document.createElement('img');
-  const year = document.createElement('p');
-  const description = document.createElement('p');
-
-
-  title.innerText = selectedMovie.title;
-  title.image = selectedMovie.image;
-  year.innerText = selectedMovie.release_date;
-  description.innerText = selectedMovie.description;
-
-  movieDetailTag.appendChild(title);
-  movieDetailTag.appendChild(image);
-  movieDetailTag.appendChild(year);
-  movieDetailTag.appendChild(description);
+  let htmlString = '';
+  htmlString += `
+          <h3>
+            ${selectedMovie.title}
+          </h3>
+          <h4>
+          ${selectedMovie.release_date}
+        </h4>
+        <p>
+        ${selectedMovie.description}
+      </p>
+      `;
+  document.getElementById('movie-details').innerHTML = htmlString;
 
 };
 
+//WATCHED MOVIES ///
 
-// COMMENTS
-const addListItem = (text) => {
-  const li = document.createElement('li');
-  li.innerText = text;
-  document.querySelector('ul').appendChild(li);
+// ALPHABETICAL SORT///
+const sortByAlphabet = (a, b) => {
+  if (a.title > b.title) {
+    return 1;
+  }
+  if (a.title < b.title) {
+    return -1;
+  }
 };
+
+//WATCHED MOVIES DETAILS ///
+async function watchedMovies() {
+  watchedMoviesCollection.onSnapshot((querySnapshot) => {
+    let htmlString = '';
+    const watchedMovie = convertQuerySnapshotToRegularArray(querySnapshot);
+    watchedMovie.sort(sortByAlphabet);
+    watchedMovie.forEach((watchedMovie) => {
+      htmlString += `
+          <p>
+            You watched "${watchedMovie.title}" 
+          </p>
+      `;
+
+    });
+    document.getElementById('watched-movies').innerHTML = htmlString;
+  });
+}
+
+
+const watchedButton = document.getElementById("btn-watched");
+const watchedList = document.querySelector("watched-movies");
+watchedButton.addEventListener("click", watched);
+
+
+async function watched(event) {
+  console.log('clicked');
+  event.preventDefault();
+  const selectedMovie = document.getElementById("select-movies");
+  const title = selectedMovie.value;
+  console.log(selectedMovie.value);
+
+  await watchedMoviesCollection.add({
+    title: title,
+    Date: new Date()
+  });
+}
